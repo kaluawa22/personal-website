f29c10c015fb571f0bcdf8fe44decb00dbe90c03
kaluawa22 <kaluawa55555@gmail.com>
2020-11-19 14:50:07 -0600

    added images

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
@@ -9,4 +11,4 @@ urlpatterns = [
     url(r'^resume/$', views.resume, name='resume'),
     url(r'^memory_game/$', views.memory_game, name='memory_game'),
     url(r'^download/(?P<filename>.*)$', views.download_zip, name='download_zip'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
